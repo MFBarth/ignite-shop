f52d095829aee4554ee7b40ae5aeb974a6a6ba50
Matheus Barth <matheusbarth@MacBook-Pro-de-Matheus.local>
2023-03-20 11:59:31 -0300

    feat: creating product page

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,4 +1,5 @@
 import Image from "next/image"
+import Link from "next/link";
 import { GetStaticProps } from "next";
 import Stripe from "stripe"
 import { useKeenSlider } from 'keen-slider/react';
@@ -10,7 +11,7 @@ interface HomeProps {
     id: string,
     name: string,
     imageUrl: string,
-    price: number
+    price: string,
   }[]
 }
 
@@ -25,14 +26,16 @@ export default function Home({ products }: HomeProps) {
   return (
     <HomeContainer ref={sliderRef} className="keen-slider">
       {products.map(product => (
-        <Product key={product.id} className="keen-slider__slide">
-          <Image src={product.imageUrl} width={520} height={480} alt="" />
+        <Link href={`/product/${product.id}`} key={product.id}>
+          <Product className="keen-slider__slide">
+            <Image src={product.imageUrl} width={520} height={480} alt="" />
 
-          <footer>
-            <strong>{product.name}</strong>
-            <span>{product.price}</span>
-          </footer>
-        </Product>
+            <footer>
+              <strong>{product.name}</strong>
+              <span>{product.price}</span>
+            </footer>
+          </Product>
+        </ Link>
       ))}
     </HomeContainer>
   )
